a3deaa41a4cd91f220f55bad0a4ff33b7945f3d6
VaheSargsyan <sargsyan.vahe@gmail.com>
2023-05-13 11:53:51 +0400

    Adds fromatting to population and area

--- src/Components/CountryItem/CountryItem.tsx
+++ src/Components/CountryItem/CountryItem.tsx
@@ -20,17 +20,24 @@ export function CountryItem({ countryData }: ICountryItemProps) {
     cca3,
   } = countryData;
 
+  const formatArea = (area: number): string => {
+    if (area < 1000000) {
+      return area.toLocaleString();
+    } else {
+      const millionArea = (area / 1000000).toFixed(3);
+      return millionArea.toLocaleString() + ' million';
+    }
+  };
+
   return (
     <tr className="country-item">
       <td>{common}</td>
       <td>{region}</td>
-      <td>{population}</td>
-      <td>{area}</td>
+      <td>{population.toLocaleString()}</td>
+      <td>{formatArea(area)} km<sup>2</sup></td>
       <td>{flag}</td>
       <td>
-        <div onClick={() => onDeleteCountry(cca3)} className="country-item__delete-btn">
-    
-        </div>
+        <div onClick={() => onDeleteCountry(cca3)} className="country-item__delete-btn"/>
       </td>
     </tr>
   );
